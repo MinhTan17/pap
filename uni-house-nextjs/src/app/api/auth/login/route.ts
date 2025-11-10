9d02418932d89dd89a306ffee4664948505a72bb
MinhTan17 <tanvhmps39189@gmail.com>
2025-11-10 14:24:36 +0700

    fix

--- uni-house-nextjs/src/app/api/auth/login/route.ts
+++ uni-house-nextjs/src/app/api/auth/login/route.ts
@@ -6,7 +6,7 @@ import { checkRateLimit, resetRateLimit } from '@/lib/rate-limit';
 
 const ADMIN_USERNAME = 'admin';
 // Password: admin123
-const ADMIN_PASSWORD_HASH = '$2b$10$bBhSEEFGNDkGYv8x0SLj6uTdPl81xM1QrMIcMAHafrJHnuzAjdn5q';
+const ADMIN_PASSWORD_HASH = '$2b$10$mH.saPf2K4ZpHBs7llxNFeFWANblzh4YJF9lxkqdgQ5t.bcw0u9mC';
 
 export async function POST(request: Request) {
   try {
